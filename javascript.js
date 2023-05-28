8de8f53169c9baeeaf4b4b17c6234a5c6e7d0647
dustin <109438275+doubledusty@users.noreply.github.com>
2023-05-28 14:51:09 -0400

    added box and started div grid generation

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+let xBoxes = 16
+let yBoxes = 16
+
+const sketchBox = document.getElementById("sketchBox")
+
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        let newDiv = document.createElement("div")
+        
+    }
+    document.create
+}
