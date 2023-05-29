06b2362996cc619c332a97aa5b2a9a8650eaa213
dustin <109438275+doubledusty@users.noreply.github.com>
2023-05-29 12:11:27 -0400

    slider working with color change now as js style change

--- javascript.js
+++ javascript.js
@@ -1,21 +1,36 @@
-
-let dimension = 100
-
-const sketchBox = document.getElementById("sketchBox")
-
-for (i = 0; i < dimension; i++) {
-    let newRow = document.createElement("div")
-    newRow.className = "row"
-    sketchBox.appendChild(newRow)
-
-    for (j = 0; j < dimension; j++) {
-        let newSquare = document.createElement("div")
-        newSquare.className = "off square"
-        newSquare.addEventListener("mouseover", function() {
-            newSquare.className = "on square"
-        })
-        newRow.appendChild(newSquare)
-
-    }
+function generateSquares(dimension) {
+    for (i = 0; i < dimension; i++) {
+        let newRow = document.createElement("div")
+        newRow.className = "row"
+        sketchBox.appendChild(newRow)
     
+        for (j = 0; j < dimension; j++) {
+            let newSquare = document.createElement("div")
+            newSquare.className = "square"
+            newSquare.addEventListener("mouseover", function() {
+                newSquare.style.backgroundColor = "black"
+            })
+            newRow.appendChild(newSquare)
+    
+        }
+        
+    }
 }
+
+const slider = document.getElementById("slider")
+const sketchBox = document.getElementById("sketchBox")
+const select = document.getElementById("select")
+const label = document.getElementById("label")
+label.innerHTML = slider.value
+generateSquares(slider.value)
+//update text with current slider position
+slider.addEventListener("input", function(){
+    label.textContent = this.value
+})
+
+select.addEventListener("click", function(){
+    sketchBox.textContent = ""
+    generateSquares(slider.value)
+})
+
+
